e6705a39c09d02c6b4f53fe0a36bdaea0316c29f
danilo.meneghel <danilo.meneghel@gmail.com>
2018-05-07 16:10:55 -0300

    Envio de arquivos

---
+++ README.md
@@ -0,0 +1,54 @@
+# APP Contatos com Ionic
+
+Projeto de um APP de Contatos usando o Framework Ionic com banco de dados Firebase.
+
+## Características
+
+- CRUD Contatos
+
+## Tecnologias
+
+- Ionic
+- AngularJS
+- NodeJS
+- Firebase
+
+## Instalação
+
+Rode o comando abaixo para instalar as dependências:
+
+```bash
+$ npm install
+```
+
+Após, rode o projeto no navegador
+
+```bash
+$ ionic serve
+```
+
+Ou rode no seu sistema
+
+Android
+
+```bash
+$ ionic cordova platform add android
+$ ionic cordova run android
+```
+
+iOS
+
+```bash
+$ ionic cordova platform add ios
+$ ionic cordova run ios
+```
+
+## License
+
+APP Contato está licenciado sobre <a href="LICENSE">The MIT License (MIT)</a>.
+
+## Screenshots
+
+![Screenshots](screenshots/screenshot01.png)<br><br>
+![Screenshots](screenshots/screenshot02.png)<br><br>
+![Screenshots](screenshots/screenshot03.png)<br><br>
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ContactProvider } from '../providers/contact/contact';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCaGbMeYOCoAsHoPe8baS-r1T5pAUb4LEQ",
+    authDomain: "contact-8a885.firebaseapp.com",
+    databaseURL: "https://contact-8a885.firebaseio.com",
+    projectId: "contact-8a885",
+    storageBucket: "contact-8a885.appspot.com",
+    messagingSenderId: "139058202804"
+    }),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
+  ]
+})
+export class AppModule {}
